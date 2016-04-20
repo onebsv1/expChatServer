440942b6322f8c4bd1dbaac675db020d78e359cf
Bhargav Srinivasan <bhargavsvasan@hotmail.com>
2016-04-20 01:28:55 -0400

    Chat Server v1.0

---
+++ src/com/chatbsv/Client.java
@@ -0,0 +1,67 @@
+package com.chatbsv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+import static java.lang.System.exit;
+
+/**
+ * Created by Bhargav Srinivasan on 4/13/16.
+ */
+public class Client {
+
+    public static void main(String args[]){
+
+        Integer clientPort = 24002;
+        String userInput;
+
+        try(Socket clientSocket = new Socket()){
+
+            InetAddress serverAddr = InetAddress.getByName(null);
+            SocketAddress serverSockAddr = new InetSocketAddress(serverAddr,clientPort);
+
+            //Try to connect to the server within 1 sec.
+            try {
+                clientSocket.connect(serverSockAddr,1000);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+
+            BufferedReader is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            PrintStream os = new PrintStream(clientSocket.getOutputStream());
+
+
+            while (true){
+
+                while(is.ready()) {
+                    System.out.println(is.readLine());
+                }
+
+                System.out.print("Client:    ");
+
+                if((userInput = stdIn.readLine()) != null) {
+                    if(userInput.equals("EXIT")){
+                        clientSocket.close();
+                        exit(0);
+                    }
+                    os.println(userInput);
+                    os.flush();
+                }
+
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ src/com/chatbsv/Server.java
@@ -0,0 +1,162 @@
+package com.chatbsv;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+
+/**
+ * Created by Bhargav Srinivasan on 4/13/16.
+ */
+public class Server {
+
+    private static class serverThread implements Runnable {
+
+        private Socket clientSocket;
+        private ConcurrentLinkedDeque<Socket> clientSockets;
+        String line;
+
+        serverThread(Socket clientSock, ConcurrentLinkedDeque<Socket> clientSockets){
+            this.clientSocket = clientSock;
+            this.clientSockets = clientSockets;
+        }
+
+
+        @Override
+        public void run() {
+
+            try {
+                BufferedReader is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                while (clientSocket.isConnected()) {
+                    try {
+                        line = is.readLine();
+                        for (Socket clientSock : clientSockets) {
+                            if(clientSock.isConnected()){
+                                PrintStream os = new PrintStream(clientSock.getOutputStream());
+                                if(!line.isEmpty()){
+                                    os.println("From Server: " + line);
+                                }
+                                os.flush();
+                            }
+                        }
+                        if(clientSocket.isInputShutdown() || line.isEmpty()){
+                            Thread.sleep(100);
+                            break;
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    catch (NullPointerException n){
+                        clientSocket.close();
+                        return;
+                    }
+
+                }
+
+                clientSocket.close();
+
+            } catch (IOException e1){
+                e1.printStackTrace();
+            }
+
+        }
+    }
+
+    private static void pollConnections(ConcurrentLinkedDeque<Thread> socketThreads){
+
+        for (Thread sockThread : socketThreads) {
+            System.out.println(sockThread.getName() + " isAlive status: " + sockThread.isAlive());
+        }
+
+    }
+
+    private static void reclaimUnusedConnections(ConcurrentLinkedDeque<Thread> socketThreads, ConcurrentHashMap<Thread, Socket> clientSockThread, ConcurrentLinkedDeque<Socket> clientSockets){
+
+        try {
+            for (Thread sockThread: socketThreads) {
+                sockThread.join(10);
+                if(!sockThread.isAlive()){
+                    socketThreads.remove(sockThread);
+                    clientSockets.remove(clientSockThread.get(sockThread));
+                    clientSockThread.remove(sockThread);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void acceptNewIncomingConnections(ConcurrentLinkedDeque<Thread> socketThreads, ConcurrentLinkedDeque<Socket> clientSockets, ConcurrentHashMap<Thread, Socket> clientSockThread, ServerSocket serverSocket, Integer connectionLimit){
+
+        try {
+            while (socketThreads.size() < connectionLimit) {
+                Socket clientSocket = serverSocket.accept();
+                clientSockets.add(clientSocket);
+                socketThreads.add(new Thread(new serverThread(clientSocket,clientSockets)));
+                clientSockThread.put(socketThreads.getLast(), clientSocket);
+                socketThreads.getLast().start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void main(String args[]) {
+
+        Integer socketPort = 24002;
+        Integer connectionLimit = 3;
+        Integer serverIteration = 1;
+        ConcurrentLinkedDeque<Thread> socketThreads = new ConcurrentLinkedDeque<>();
+        ConcurrentLinkedDeque<Socket> clientSockets = new ConcurrentLinkedDeque<>();
+        ConcurrentHashMap<Thread,Socket> clientSockThread = new ConcurrentHashMap<>();
+        boolean reclaimed;
+
+
+        try (ServerSocket serverSocket = new ServerSocket(socketPort)) {
+            System.out.println("Server has been started, press Ctrl+C to exit...");
+
+            for (int i=0; i < connectionLimit; i++){
+                Socket clientSocket = serverSocket.accept();
+                clientSockets.add(clientSocket);
+                socketThreads.add(new Thread(new serverThread(clientSocket,clientSockets)));
+                clientSockThread.put(socketThreads.getLast(),clientSocket);
+                socketThreads.getLast().start();
+            }
+
+
+            while (true){
+                System.out.println("Iteration #"+serverIteration);
+
+                pollConnections(socketThreads);
+                reclaimUnusedConnections(socketThreads,clientSockThread,clientSockets);
+                reclaimed = (socketThreads.size() < connectionLimit);
+
+                if(reclaimed) {
+                    // blocks until all of the reclaimed sockets have been used.
+                    acceptNewIncomingConnections(socketThreads, clientSockets, clientSockThread, serverSocket, connectionLimit);
+                }
+
+                try{
+                    Thread.sleep(1000);
+                } catch (InterruptedException e2) {
+                    e2.printStackTrace();
+                }
+
+                serverIteration = (serverIteration>20000)?1:serverIteration+1;
+            }
+
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+    }
+
+
+}
